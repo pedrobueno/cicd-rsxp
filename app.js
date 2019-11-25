55576bef7a090c57b9282ae9eecd3ba69f03fa12
Pedro Bueno <pedro@mentores.com.br>
2019-11-25 18:14:34 -0300

    fix lint

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var app = express;
+var express = require('express')
+var app = express
 
-app.get('/', (req, res) => ({ msg: 'asdfasdf'}));
+app.get('/', (req, res) => ({ msg: 'asdfasdf' }))
 
-app.listen(3000, () => console.log('Ouvindo porta 30000'));
+app.listen(3000, () => console.log('Ouvindo porta 30000'))
